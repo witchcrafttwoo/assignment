4599cacf7679f6926fddea6ff89eb0ef7480c148
witchcrafttwoo <bsi25009@uhe.ac.jp>
2025-09-29 13:15:09 +0900

    わあ

--- java1-2/src/java2/Exp9_Catpetshop.java
+++ java1-2/src/java2/Exp9_Catpetshop.java
@@ -1,5 +1,16 @@
 package java2;
 
 public class Exp9_Catpetshop {
+	public static void main (String []args) {
+		Exp9_Catstate cat1 = new Exp9_Catstate();
+		cat1.name = "EVA";
+		cat1.bloodtype = "A";
+		cat1.Runawaymode = true;
+		cat1.age = 0;
+		cat1.weight = 50000;
+		
+		System.out.println("名前"+ cat1.name);
+				
+	}
 
 }
--- java1-2/src/java2/Exp9_Catstate.java
+++ java1-2/src/java2/Exp9_Catstate.java
@@ -1,5 +1,9 @@
 package java2;
 
 public class Exp9_Catstate {
-
+	String name;
+	int age;
+	int weight;
+	String  bloodtype;
+	boolean Runawaymode;
 }
